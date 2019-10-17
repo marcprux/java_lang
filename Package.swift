e308632de721a79119b4a76e3b62faf5893d4cad
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-10-17 16:39:26 -0500

    Updated for Swift 5

--- Package.swift
+++ Package.swift
@@ -1,22 +1,27 @@
-//
-//  Package.swift
-//  SwiftJava
-//
-//  Created by John Holdsworth on 20/07/2016.
-//  Copyright (c) 2016 John Holdsworth. All rights reserved.
-//
-
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "java_lang",
+    products: [
+        .library(
+            name: "java_lang",
+            targets: ["java_lang"]
+        ),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/SwiftJava/java_swift.git", versions: Version(2,1,0)..<Version(3,0,0)),
-//        Package(url: "https://github.com/SwiftJava/java_util.git", versions: Version(1,0,0)..<Version(2,0,0)),
-//        Package(url: "https://github.com/SwiftJava/java_io.git", versions: Version(1,0,0)..<Version(2,0,0)),
-//        Package(url: "https://github.com/SwiftJava/java_net.git", versions: Version(1,0,0)..<Version(2,0,0)),
-//        Package(url: "https://github.com/SwiftJava/java_security.git", versions: Version(1,0,0)..<Version(2,0,0)),
-//        Package(url: "https://github.com/SwiftJava/javax_management.git", versions: Version(1,0,0)..<Version(2,0,0)),
-//        Package(url: "https://github.com/SwiftJava/java_nio.git", versions: Version(1,0,0)..<Version(2,0,0)),
-        ]
+        .package(
+            url: "https://github.com/PureSwift/java_swift.git",
+            .branch("master")
+        ),
+    ],
+    targets: [
+        .target(
+            name: "java_lang",
+            dependencies: [
+                "java_swift",
+            ],
+            path: "./Sources"
+        )
+    ]
 )
